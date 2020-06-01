b373624afc0aeb46c9491d6130dd849de39f11c1
Himel Das <himelhub@gmail.com>
2020-06-01 07:15:26 +0600

    refactor

---
+++ src/birthday_chocolate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+birthday(vector<int> s, int d, int m)
+{
+    int count=0;
+    for(i=0; i<n-1; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    birthday({1, 2, 1, 3, 2}, 3, 2)
+    return 0;
+}
